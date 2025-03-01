740288bcf740d5a6c11e2a87572cd97a14efd9c0
yashr775 <rajy29629@gmail.com>
2025-03-01 16:16:56 +0530

    Home page completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 const Navbar = () => {
     return (
         <div className="bg-black w-full h-14 flex justify-center items-center">
+
             <ul className="flex text-xl space-x-10 text-white font-bold">
                 <li className="py-2 px-4 hover:text-gray-400 cursor-pointer">Home</li>
                 <li className="py-2 px-4 hover:text-gray-400 cursor-pointer">About</li>

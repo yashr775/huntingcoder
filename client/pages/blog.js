ae00f0bf1348955e5d0d6803e35d362d5788713c
yashr775 <rajy29629@gmail.com>
2025-03-07 22:36:11 +0530

    blog post page completed

--- client/pages/blog.js
+++ client/pages/blog.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Link from "next/link";
 
 const Blog = () => {
     return (
         <div className="blog-container flex justify-center min-h-screen bg-gray-900 text-white">
             <div className="blogs flex flex-col  items-center mt-20 w-full max-w-2xl p-6">
-                <h1 className="text-3xl font-bold mb-2">How to learn Next.js in 2025?</h1>
+                <h1 className="text-3xl font-bold mb-2"><Link href="/blogpost/learnjavascript">How to learn Next.js in 2025?</Link></h1>
                 <p className="mb-5">Next.js is a framework built over React.</p>
 
                 <h1 className="text-3xl font-bold mb-2">How to master Next.js?</h1>
--- client/pages/blogpost/[slug].js
+++ client/pages/blogpost/[slug].js
@@ -1,13 +1,29 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React from "react";
+import { useRouter } from "next/router";
 
 const Slug = () => {
-
     const router = useRouter();
-    const { slug } = router.query
+    const { slug } = router.query;
     return (
-        <div>{slug}</div>
-    )
-}
+        <div className="flex flex-col items-center">
+            <h1 className="font-bold text-3xl mt-10">Title of this page {slug}</h1>
+            <div className="max-w-3/4 mt-10 text-lg">
+                A black hole is a massive, compact astronomical object so dense that its
+                gravity prevents anything from escaping, even light. Albert Einstein
+                theory of general relativity predicts that a sufficiently compact mass
+                will form a black hole. The boundary of no escape is called the event
+                horizon. A black hole has a great effect on the fate and circumstances
+                of an object crossing it, but has no locally detectable features
+                according to general relativity. In many ways, a black hole acts like
+                an ideal black body, as it reflects no light. Quantum field theory
+                in curved spacetime predicts that event horizons emit Hawking radiation,
+                with the same spectrum as a black body of a temperature inversely
+                proportional to its mass. This temperature is of the order of billionths
+                of a kelvin for stellar black holes, making it essentially impossible to
+                observe directly.
+            </div>
+        </div>
+    );
+};
 
-export default Slug
+export default Slug;

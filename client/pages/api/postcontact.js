06fe8b033ce21504a94fca22e6d65ce6d80fa75b
yashr775 <rajy29629@gmail.com>
2025-03-18 20:09:05 +0530

    Post contact completed

--- client/pages/api/postcontact.js
+++ client/pages/api/postcontact.js
@@ -1,5 +1,10 @@
-export default function handler(req, res) {
+import * as fs from "fs"
+
+export default async function handler(req, res) {
     if (req.method === "POST") {
+        let data = await fs.promises.readdir('contactdata');
+        fs.promises.writeFile(`contactdata/${data.length + 1}.json`, JSON.stringify(req.body))
+        res.status(200).json(req)
 
     } else {
         res.status(200).send("Allblogs")

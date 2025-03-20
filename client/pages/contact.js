78cd586ebf84750d6e6a6584e5846878d3696c55
yashr775 <rajy29629@gmail.com>
2025-03-20 20:53:25 +0530

    EOD commit

--- client/pages/contact.js
+++ client/pages/contact.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useState } from 'react'
+
 
 const Contact = () => {
+
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [phone, setPhone] = useState();
+    const [desc, setDesc] = useState();
+
+
 
 
     return (
@@ -12,49 +20,39 @@ const Contact = () => {
                 <form className="space-y-4 font-[sans-serif] text-[#333] max-w-md mx-auto" >
                     <div className="relative flex items-center">
                         <input
+                            type="text"
+                            placeholder="Enter Name"
+                            name="name"
+                            className="px-4 py-3 bg-[#f0f1f2] focus:bg-transparent w-full text-sm border outline-[#007bff] rounded transition-all"
+                        />
+
+                    </div>
+                    <div className="relative flex items-center">
+                        <input
                             type="email"
                             placeholder="Enter Email"
                             name="email"
                             className="px-4 py-3 bg-[#f0f1f2] focus:bg-transparent w-full text-sm border outline-[#007bff] rounded transition-all"
                         />
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="#bbb"
-                            stroke="#bbb"
-                            className="w-[18px] h-[18px] absolute right-4"
-                            viewBox="0 0 682.667 682.667"
-                        >
-                            <defs>
-                                <clipPath id="a" clipPathUnits="userSpaceOnUse">
-                                    <path d="M0 512h512V0H0Z" data-original="#000000"></path>
-                                </clipPath>
-                            </defs>
-                            <g
-                                clipPath="url(#a)"
-                                transform="matrix(1.33 0 0 -1.33 0 682.667)"
-                            >
-                                <path
-                                    fill="none"
-                                    strokeMiterlimit="10"
-                                    strokeWidth="40"
-                                    d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z"
-                                    data-original="#000000"
-                                ></path>
-                                <path
-                                    d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z"
-                                    data-original="#000000"
-                                ></path>
-                            </g>
-                        </svg>
+
                     </div>
 
                     <div className="relative flex items-center">
                         <input
-                            type="text"
+                            type="password"
                             placeholder="Enter Password"
                             name="password"
                             className="px-4 py-3 bg-[#f0f1f2] focus:bg-transparent w-full text-sm border outline-[#007bff] rounded transition-all"
                         />
+                    </div>
+                    <div className="relative flex items-center">
+                        <input
+                            type="text"
+                            placeholder="Elaborate your concern"
+                            name="comments"
+                            className="px-4 py-3 bg-[#f0f1f2] focus:bg-transparent w-full text-sm border outline-[#007bff] rounded transition-all"
+                        />
+
                     </div>
 
                     <button
